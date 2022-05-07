3c03f57de1f1c17c93fffcb8d581fbba16e4ade2
Yining Wang <ywang@socialsolutions.com>
2022-05-06 21:42:09 -0500

    wrapped map component

--- src/components/WrappedGoogleMap/WrappedGoogleMap.js
+++ src/components/WrappedGoogleMap/WrappedGoogleMap.js
@@ -1,4 +1,3 @@
-import { formatMs } from "@material-ui/core";
 import React, { useEffect, useRef } from "react";
 import {
 	withScriptjs,
@@ -7,20 +6,21 @@ import {
 	Marker,
 } from "react-google-maps";
 
+import "./styles.css";
+
 export const WrappedGoogleMap = ({ zoom, center }) => {
-	const GoogleMapWithScripts = withScriptjs(
-		withGoogleMap(() => (
+	const map = () => {
+		return (
 			<GoogleMap defaultZoom={zoom} defaultCenter={center}>
-				{/* <Marker position={{ lat: 30.5083, lng: 97.6789 }}></Marker> */}
+				<Marker position={center}></Marker>
 			</GoogleMap>
-		))
-	);
+		);
+	};
+	const GoogleMapWithScripts = withScriptjs(withGoogleMap(map));
 
 	return (
 		<GoogleMapWithScripts
-			googleMapURL={
-				"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBuBvQjnLRAYFSB5546s_miv4KR5JYM0wM"
-			}
+			googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
 			loadingElement={<div style={{ height: "100%" }} />}
 			containerElement={<div style={{ height: "100%" }} />}
 			mapElement={<div style={{ height: "100%" }} />}
