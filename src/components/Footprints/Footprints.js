cc306e0bc46b590bea012d4c5d3358ee529b345b
Yining Wang <ywang@socialsolutions.com>
2022-05-06 21:41:38 -0500

    added slider and map to footprints page

--- src/components/Footprints/Footprints.js
+++ src/components/Footprints/Footprints.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { render } from "@testing-library/react";
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 import { GoogleMap, Marker } from "react-google-maps";
 import axios from "axios";
 
@@ -8,14 +9,61 @@ import "./styles.css";
 
 export const Footprints = () => {
 	const ref = React.useRef(null);
-	const [map, setMap] = React.useState();
+	const [zoom, setZoom] = React.useState(4);
+	const [coordinates, setCoordinates] = React.useState({
+		lat: 37.863,
+		lng: 112.548,
+	});
+	const [currentMarker, setCurrentMarker] = React.useState(1994);
+
+	const footprintsCoordinates = {
+		1994: { lat: 37.863, lng: 112.548 },
+		2012: { lat: 43.258, lng: -79.905 },
+		2013: { lat: 43.661, lng: -79.395 },
+		2017: { lat: 30.285, lng: -97.739 },
+		2022: { lat: 30.285, lng: -97.739 },
+	};
+
+	const onSliderValueChange = (value) => {
+		const years = Object.keys(footprintsCoordinates).map((strYear) =>
+			parseInt(strYear)
+		);
+
+		console.log(Math.min(...years));
+
+		if (Math.min(...years) <= value <= Math.max(...years)) {
+			for (let i = 0; i < years.length; i++) {
+				if (value >= years[i] && value < years[i + 1]) {
+					if (years[i] !== currentMarker) {
+						setCoordinates(footprintsCoordinates[years[i]]);
+						setCurrentMarker(years[i]);
+					}
+				}
+			}
+		}
+	};
 
 	return (
-		<div>
-			<div>hello a</div>
-			<WrappedGoogleMap zoom={2} center={{ lat: 30.5083, lng: 97.6789 }}>
-				hello a
-			</WrappedGoogleMap>
+		<div className="pageContainer">
+			<div className="controllerContainer">
+				<div className="sliderContainer">
+					<Slider
+						min={1994}
+						max={2022}
+						marks={{
+							1994: 1994,
+							2012: 2012,
+							2013: 2013,
+							2017: 2017,
+							2022: 2022,
+						}}
+						onChange={(value) => onSliderValueChange(value)}
+					></Slider>
+				</div>
+			</div>
+			<div className="mapContainer">
+				<WrappedGoogleMap zoom={zoom} center={coordinates}></WrappedGoogleMap>
+			</div>
 		</div>
 	);
 };
