e1784221ec48aa9d2e8eb5d32ff218b45ebe3651
Yining Wang <ywang@socialsolutions.com>
2022-05-14 16:18:53 -0500

    minor update on photography style

--- src/components/Photography/Photography.js
+++ src/components/Photography/Photography.js
@@ -7,113 +7,113 @@ import { Modal } from "../Modal/Modal.js";
 import "./styles.css";
 
 export const Photography = () => {
-  const baseAPIUrl =
-    "https://yining-wang-portfolio-photo.herokuapp.com/api/v1/photos/";
+	const baseAPIUrl =
+		"https://yining-wang-portfolio-photo.herokuapp.com/api/v1/photos/";
 
-  const [category, setCategory] = useState("");
-  const [initial, setInitial] = useState(true);
-  const [photos, setPhotos] = useState([]);
-  const [inspectedPhoto, setInspectedPhoto] = useState({});
-  const [inspectedPhotoUrl, setInspectedPhotoUrl] = useState("");
-  const [allowScroll, setAllowScroll] = useState(true);
+	const [category, setCategory] = useState("");
+	const [initial, setInitial] = useState(true);
+	const [photos, setPhotos] = useState([]);
+	const [inspectedPhoto, setInspectedPhoto] = useState({});
+	const [inspectedPhotoUrl, setInspectedPhotoUrl] = useState("");
+	const [allowScroll, setAllowScroll] = useState(true);
 
-  const categories = {
-    Home: "home",
-    Animals: "animals",
-    Plants: "plants",
-    Civilization: "civilization",
-    Lights: "lights",
-    "Man vs. Wild": "man_wild",
-    Mountains: "mountains",
-    Homed: "homed"
-  };
+	const categories = {
+		Home: "home",
+		Animals: "animals",
+		Plants: "plants",
+		Civilization: "civilization",
+		Lights: "lights",
+		"Man vs. Wild": "man_wild",
+		Mountains: "mountains",
+		Homed: "homed",
+	};
 
-  const chooseCategory = cat => {
-    setCategory(cat);
-    axios
-      .get(`${baseAPIUrl}${categories[cat]}`)
-      .then(res => res.data)
-      .then(data => setPhotos(data));
-  };
+	const chooseCategory = (cat) => {
+		setCategory(cat);
+		axios
+			.get(`${baseAPIUrl}${categories[cat]}`)
+			.then((res) => res.data)
+			.then((data) => setPhotos(data));
+	};
 
-  const inspectPhoto = (photo, photoUrl) => {
-    setInspectedPhoto(photo);
-    setInspectedPhotoUrl(photoUrl);
-    setAllowScroll(false);
-  };
+	const inspectPhoto = (photo, photoUrl) => {
+		setInspectedPhoto(photo);
+		setInspectedPhotoUrl(photoUrl);
+		setAllowScroll(false);
+	};
 
-  const closeModal = () => {
-    setInspectedPhoto({});
-    setInspectedPhotoUrl("");
-    setAllowScroll(true);
-  };
+	const closeModal = () => {
+		setInspectedPhoto({});
+		setInspectedPhotoUrl("");
+		setAllowScroll(true);
+	};
 
-  useEffect(() => {
-    axios
-      .get(`${baseAPIUrl}home`)
-      .then(res => res.data)
-      .then(data => {
-        setPhotos(data);
-        setCategory("Home");
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`${baseAPIUrl}home`)
+			.then((res) => res.data)
+			.then((data) => {
+				setPhotos(data);
+				setCategory("Home");
+			});
+	}, []);
 
-  return (
-    <div
-      className="gallery"
-      style={{
-        overflow: allowScroll ? "auto" : "hidden"
-      }}
-    >
-      {console.log(category)}
-      <div className="gallery-nav">
-        <h2>Categories</h2>
-        <ul>
-          {Object.keys(categories).map(cat => (
-            <li>
-              <a
-                className={`category-button${
-                  category === cat ? " active" : ""
-                }`}
-                onClick={() => chooseCategory(cat)}
-              >
-                {cat}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="grid-cols-3">
-        {photos.map(photo => {
-          let photoUrlStyle = `url(${`https://s3.us-east-2.amazonaws.com/yiningwang.io/${photo.filename}`})`;
-          let photoUrl = `https://s3.us-east-2.amazonaws.com/yiningwang.io/${photo.filename}`;
-          return (
-            <div>
-              <div
-                className="grid-item"
-                style={{
-                  width: "100%",
-                  height: "300px",
-                  // margin: "0.5vw",
-                  border: "solid 4px white",
-                  paddingBottom: "100%",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                  backgroundImage: `${photoUrlStyle}`
-                }}
-                onClick={() => inspectPhoto(photo, photoUrl)}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <Modal
-        display={Object.keys(inspectedPhoto).length === 0 ? false : true}
-        inspectedPhoto={inspectedPhoto}
-        photoUrl={inspectedPhotoUrl}
-        close={closeModal}
-      ></Modal>
-    </div>
-  );
+	return (
+		<div
+			className="gallery"
+			style={{
+				overflow: allowScroll ? "auto" : "hidden",
+			}}
+		>
+			<div className="gallery-nav">
+				<h2>Categories</h2>
+				<ul>
+					{Object.keys(categories).map((cat) => (
+						<li>
+							<a
+								className={`category-button${
+									category === cat ? " active" : ""
+								}`}
+								onClick={() => chooseCategory(cat)}
+							>
+								{cat}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="grid-cols-3">
+				{photos.map((photo) => {
+					let photoUrlStyle = `url(${`https://s3.us-east-2.amazonaws.com/yiningwang.io/${photo.filename}`})`;
+					let photoUrl = `https://s3.us-east-2.amazonaws.com/yiningwang.io/${photo.filename}`;
+					return (
+						<div>
+							<div
+								className="grid-item"
+								style={{
+									width: "100%",
+									height: "300px",
+									border: "solid 4px white",
+									borderRadius: "10%",
+									overflow: "hidden",
+									paddingBottom: "100%",
+									backgroundPosition: "center",
+									backgroundRepeat: "no-repeat",
+									backgroundSize: "cover",
+									backgroundImage: `${photoUrlStyle}`,
+								}}
+								onClick={() => inspectPhoto(photo, photoUrl)}
+							/>
+						</div>
+					);
+				})}
+			</div>
+			<Modal
+				display={Object.keys(inspectedPhoto).length === 0 ? false : true}
+				inspectedPhoto={inspectedPhoto}
+				photoUrl={inspectedPhotoUrl}
+				close={closeModal}
+			></Modal>
+		</div>
+	);
 };
