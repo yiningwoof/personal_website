08408aa77ec4b0df024a41b7dab92b3161d3b217
Yining Wang <yining.wang@weinfuse.com>
2023-07-30 22:23:51 -0500

    hide footprints

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,45 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import resume from "resume.pdf";
 
-import "./styles.css";
+import './styles.css';
 
 export const Nav = ({ home }) => {
   return (
     <nav
       style={
-        !home ? { backgroundColor: "#0006" } : { backgroundColor: "#0000" }
+        !home ? { backgroundColor: '#0006' } : { backgroundColor: '#0000' }
       }
     >
       <ul>
-        <Link id={"nav__home-link"} className={"nav__link"} to="/">
-          <li color="inherit">Home</li>
+        <Link id={'nav__home-link'} className={'nav__link'} to='/'>
+          <li color='inherit'>Home</li>
         </Link>
-        <Link id={"nav__resume-link"} className={"nav__link"} to="/about">
+        <Link id={'nav__resume-link'} className={'nav__link'} to='/about'>
           About
         </Link>
         <Link
-          id={"nav__project-link"}
-          className={"nav__link"}
-          to="/tech_projects"
+          id={'nav__project-link'}
+          className={'nav__link'}
+          to='/tech_projects'
         >
-          <li color="inherit">Tech Projects</li>
+          <li color='inherit'>Tech Projects</li>
         </Link>
         <Link
-          id={"nav__photography-link"}
-          className={"nav__link"}
-          to="/photography"
+          id={'nav__photography-link'}
+          className={'nav__link'}
+          to='/photography'
         >
-          <li color="inherit">Photography</li>
+          <li color='inherit'>Photography</li>
         </Link>
-        <Link
+        {/* <Link
           id={"nav__footprints-link"}
           className={"nav__link"}
           to="/footprints"
         >
           <li color="inherit">Footprints</li>
-        </Link>
+        </Link> */}
       </ul>
     </nav>
   );
